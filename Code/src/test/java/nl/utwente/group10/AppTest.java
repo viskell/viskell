2d4dad1f1db317a1876c101311fc53058486bc3f
Wander Nauta <wnauta@antagonist.nl>
2015-02-13 14:54:27 +0100

    Add initial Maven pom.xml

---
+++ Code/src/test/java/nl/utwente/group10/AppTest.java
@@ -0,0 +1,11 @@
+package nl.utwente.group10;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void rigorousTest() {
+        assertTrue(true);
+    }
+}
