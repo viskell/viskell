47fcaffc86d2fb265eee95923c0f6d33718c6d32
Wander Nauta <info@wandernauta.nl>
2015-02-19 13:08:16 +0100

    Make Type class abstract

--- Code/src/main/java/nl/utwente/group10/haskell/type/Type.java
+++ Code/src/main/java/nl/utwente/group10/haskell/type/Type.java
@@ -2,5 +2,5 @@ package nl.utwente.group10.haskell.type;
 
 import nl.utwente.group10.haskell.HaskellObject;
 
-public class Type extends HaskellObject {
+public abstract class Type extends HaskellObject {
 }
