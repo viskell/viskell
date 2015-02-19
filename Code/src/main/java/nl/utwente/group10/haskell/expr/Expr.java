e26265031dcdd21be5388f1d60be37ba6eb86ff8
Wander Nauta <info@wandernauta.nl>
2015-02-19 13:18:02 +0100

    Make Expr class abstract

--- Code/src/main/java/nl/utwente/group10/haskell/expr/Expr.java
+++ Code/src/main/java/nl/utwente/group10/haskell/expr/Expr.java
@@ -2,5 +2,5 @@ package nl.utwente.group10.haskell.expr;
 
 import nl.utwente.group10.haskell.HaskellObject;
 
-public class Expr extends HaskellObject {
+public abstract class Expr extends HaskellObject {
 }
