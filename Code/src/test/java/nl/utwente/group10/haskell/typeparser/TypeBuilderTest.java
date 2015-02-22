e5b4860602bba88ee160634af7e82fb7a95efa23
Wander Nauta <info@wandernauta.nl>
2015-02-22 18:10:02 +0100

    More tests for the TypeBuilder

--- Code/src/test/java/nl/utwente/group10/haskell/typeparser/TypeBuilderTest.java
+++ Code/src/test/java/nl/utwente/group10/haskell/typeparser/TypeBuilderTest.java
@@ -1,17 +1,22 @@
 package nl.utwente.group10.haskell.typeparser;
 
-import nl.utwente.group10.haskell.type.Type;
+import org.junit.Assert;
 import org.junit.Test;
 
+/** Tests the TypeBuilder */
 public class TypeBuilderTest {
-    @Test
-    public void testBuildVarT() throws Exception {
-        new TypeBuilder().build("a");
+    /**
+     * Test helper that checks if parsing a String and then converting it back
+     * gives the same result.
+     */
+    private void roundtrip(String hs) {
+        Assert.assertEquals(hs, new TypeBuilder().build(hs).toString());
     }
 
-    @Test
-    public void testBuildKitchensink() throws Exception {
-        Type result = new TypeBuilder().build("[a] -> [b] -> [(a, b)]");
-        System.out.println(result.toString());
-    }
+    @Test public void testBuildConstT() { this.roundtrip("Int"); }
+    @Test public void testBuildVarT()   { this.roundtrip("a"); }
+    @Test public void testBuildFuncT()  { this.roundtrip("(a -> b)"); }
+    @Test public void testBuildListT()  { this.roundtrip("[a]"); }
+    @Test public void testBuildTupleT() { this.roundtrip("(String, Int)"); }
+    @Test public void testKitchenSink() { this.roundtrip("([a] -> ([b] -> [(a, b)]))"); }
 }
