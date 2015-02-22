5250ad44a9a3f80081c97bb0a1ff67a2e5f92b87
Wander Nauta <info@wandernauta.nl>
2015-02-22 14:13:50 +0100

    Add parseBinding

--- Code/src/main/java/nl/utwente/group10/ghcj/GhciEnvironment.java
+++ Code/src/main/java/nl/utwente/group10/ghcj/GhciEnvironment.java
@@ -1,6 +1,9 @@
 package nl.utwente.group10.ghcj;
 
+import com.google.common.base.Splitter;
 import nl.utwente.group10.haskell.expr.Binding;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,12 +18,33 @@ public class GhciEnvironment {
      * Constructs a fresh GhciEnvironment.
      * @param ghci The parent GhciSession instance.
      */
-    public GhciEnvironment(GhciSession ghci) {
+    public GhciEnvironment(final GhciSession ghci) {
         this.ghci = ghci;
     }
 
+    /**
+     * Query the parent GHCI instance for a list of known functions.
+     * @return all functions known to GHCI as Binding instances.
+     * @throws GhciException when GHCI is not ready for our query.
+     */
     public final List<Binding> getBindings() throws GhciException {
-        System.out.println(ghci.eval(":browse"));
+        String input = this.ghci.eval(":browse");
+        List<Binding> out = new ArrayList<Binding>();
+
+        for (String line : Splitter.on('\n').split(input)) {
+            out.add(this.parseBinding(line));
+        }
+
+        return out;
+    }
+
+    /**
+     * Parses a Haskell type into a Binding.
+     * @param line A Haskell type declaration.
+     * @throws GhciException when the line is not valid.
+     * @return A Binding object matching the declaration.
+     */
+    private Binding parseBinding(final String line) throws GhciException {
         return null;
     }
 
