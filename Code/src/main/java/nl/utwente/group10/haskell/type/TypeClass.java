fc59853c5c1a5c68380aad27c7b7b2295d3883ea
Jan-Jelle Kester <janjelle@jjkester.nl>
2015-02-22 19:09:45 +0100

    Start implementation of TypeClass type

--- Code/src/main/java/nl/utwente/group10/haskell/type/TypeClass.java
+++ Code/src/main/java/nl/utwente/group10/haskell/type/TypeClass.java
@@ -1,4 +1,56 @@
 package nl.utwente.group10.haskell.type;
 
-public class TypeClass {
+import com.google.common.base.Joiner;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Haskell type class to be interpreted as type. The {@code compatibleWith} makes sure that all types supported by a
+ * type class can be used in the place where that {@code TypeClass} instance is used.
+ */
+public class TypeClass extends Type {
+    /**
+     * A list of the types within this type class.
+     */
+    private final List<Type> types;
+
+    /**
+     * @param name The name of this type class.
+     * @param types The types in this type class.
+     */
+    public TypeClass(final String name, Type ... types) {
+        super(name);
+        this.types = Arrays.asList(types);
+    }
+
+    /**
+     * Adds a type to this type class.
+     * @param type The type to add.
+     */
+    public final void addType(Type type) {
+        this.types.add(type);
+    }
+
+    @Override
+    public final boolean compatibleWith(Type other) {
+        return this.types.contains(other);
+    }
+
+    @Override
+    public final String toHaskellType() {
+        return Joiner.on("\n" + this.getName() + " ").join(this.types);
+    }
+
+    @Override
+    public final String toString() {
+        return "TypeClass{" +
+                "name='" + this.getName() + "'" +
+                "types=" + types.toArray().toString() +
+                '}';
+    }
+
+    public TypeClass clone() {
+        return new TypeClass(this.getName(), (Type[]) this.types.toArray());
+    }
 }
