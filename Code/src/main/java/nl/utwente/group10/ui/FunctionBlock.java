8bb74b81587d8e321ea407303c2401e9fba86570
TheMSB <Martijn.bruning@gmail.com>
2015-03-11 14:28:05 +0100

    Base extension of functionblock

--- Code/src/main/java/nl/utwente/group10/ui/FunctionBlock.java
+++ Code/src/main/java/nl/utwente/group10/ui/FunctionBlock.java
@@ -6,9 +6,26 @@ import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 
 public class FunctionBlock extends StackPane{
-	public static FunctionBlock newInstance() throws IOException{
+	
+	//The arguments this FunctionBlock holds
+	private String[] arguments;
+	
+	public static FunctionBlock newInstance(int numberOfArguments) throws IOException{
 		//TODO Prettier resource loading
-		FunctionBlock fuctionBlock = (FunctionBlock) FXMLLoader.load(Main.class.getResource("FunctionBlock.fxml"), null, new FunctionBlockBuilderFactory());
-		return fuctionBlock;
+		FunctionBlock functionBlock = (FunctionBlock) FXMLLoader.load(Main.class.getResource("FunctionBlock.fxml"), null, new FunctionBlockBuilderFactory());
+		functionBlock.initializeArguments(2);
+		
+		return functionBlock;
+	}
+	
+	//To be implemented method that will parse code into haskell interface
+	public void executeMethod(){
+		
+	}
+	
+	//Method to initialize the argument fields for this function block.
+	//All arguments will be stored as Strings.
+	private void initializeArguments(int numberOfArguments){
+		arguments = new String[numberOfArguments];
 	}
 }
