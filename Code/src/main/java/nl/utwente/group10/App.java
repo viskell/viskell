a6672fcbeb0154591b8783cd7a66dd275a4293b3
Wander Nauta <info@wandernauta.nl>
2015-03-14 13:28:21 +0100

    Have `mvn package` generate jar-with-dependencies
    Bundling the dependencies makes it more convenient to distribute.

--- Code/src/main/java/nl/utwente/group10/App.java
+++ Code/src/main/java/nl/utwente/group10/App.java
@@ -1,10 +1,7 @@
 package nl.utwente.group10;
 
-import nl.utwente.group10.ghcj.GhciSession;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        GhciSession session = new GhciSession();
-        session.close();
+		System.out.println("Hello, world!");
     }
 }
