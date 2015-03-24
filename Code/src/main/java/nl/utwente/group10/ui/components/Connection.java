dde0509cff91da9d2470b5804e2dc2076362cff2
TheMSB <Martijn.bruning@gmail.com>
2015-03-24 09:43:47 +0100

    refactored Connection and added Line class

--- Code/src/main/java/nl/utwente/group10/ui/components/Connection.java
+++ Code/src/main/java/nl/utwente/group10/ui/components/Connection.java
@@ -4,31 +4,25 @@ import java.io.IOException;
 
 import nl.utwente.cs.caes.tactile.fxml.TactileBuilderFactory;
 import nl.utwente.group10.ui.Main;
+import nl.utwente.group10.ui.components.Line;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.CubicCurve;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
-import javafx.scene.shape.Line;
+
 
 /**
  * This class represents a connection between two different FunctionBlocks. The
  * output of one FunctionBlock will be used as input for another FunctionBlock
  */
-public class Connection extends CubicCurve implements ChangeListener<Number> {
+public class Connection extends Line implements ChangeListener<Number> {
 
-	public static final double BEZIER_CONTROL_OFFSET_Y = 100f;
-	
-	private ConnectionAnchor startAnchor;
-	private ConnectionAnchor endAnchor;
 	/** The Block that inputs data into this connection */
 	private Block input;
 	/** The Block that we output data into from this connection */
@@ -51,28 +45,12 @@ public class Connection extends CubicCurve implements ChangeListener<Number> {
 		startAnchor.layoutYProperty().addListener(connection);
 		endAnchor.layoutXProperty().addListener(connection);
 		endAnchor.layoutYProperty().addListener(connection);
-		connection.startAnchor = startAnchor;
-		connection.endAnchor = endAnchor;			
-		connection.updateStartEndPositions();
+		
+		connection.setStartAnchor(startAnchor);
+		connection.setEndAnchor(endAnchor);	
 		
 		return connection;
 	}
-	
-	
-	public void setStartPosition(double x, double y) {
-		this.setStartX(x);
-		this.setStartY(y);
-		this.setControlX1(x);
-		this.setControlY1(y+BEZIER_CONTROL_OFFSET_Y);
-	}
-	
-	public void setEndPosition(double x, double y) {
-		this.setEndX(x);
-		this.setEndY(y);
-		this.setControlX2(x);
-		this.setControlY2(y-BEZIER_CONTROL_OFFSET_Y);
-	}
-	
 
 	/**
 	 * Private method that sets the input into this Connection
@@ -109,21 +87,7 @@ public class Connection extends CubicCurve implements ChangeListener<Number> {
 	}
 
 	@Override
-	public void changed(ObservableValue<? extends Number> observable,
-			Number oldValue, Number newValue) {
-		
+	public void changed(ObservableValue<?extends Number> observable, Number oldValue, Number newValue) {
 		updateStartEndPositions();
-	}
-	
-	private void updateStartEndPositions() {
-		updateStartPosition();
-		updateEndPosition();
-	}
-	
-	private void updateStartPosition() {
-		setStartPosition(startAnchor.getLayoutX()+startAnchor.getCenterX()-this.getLayoutX(),startAnchor.getLayoutY()+startAnchor.getCenterY()-this.getLayoutY());
-	}
-	private void updateEndPosition() {
-		setEndPosition(endAnchor.getLayoutX()+endAnchor.getCenterX()-this.getLayoutX(),endAnchor.getLayoutY()+endAnchor.getCenterY()-this.getLayoutY());
 	}
 }
---
+++ Code/src/main/java/nl/utwente/group10/ui/components/Line.java
@@ -0,0 +1,82 @@
+package nl.utwente.group10.ui.components;
+
+import javafx.scene.shape.CubicCurve;
+
+public class Line extends CubicCurve {
+
+	/** Control offset for this bezier of this line */
+	public static final double BEZIER_CONTROL_OFFSET_Y = 100f;
+	/** Starting point of this Line that can be Anchored onto other objects */
+	private ConnectionAnchor startAnchor;
+	/** Ending point of this Line that can be Anchored onto other objects */
+	private ConnectionAnchor endAnchor;
+	
+	/**
+	 * Set the startAnchor for this line.
+	 * After setting the StartPosition will be updated.
+	 * @param Anchor to start at.
+	 */
+	public void setStartAnchor(ConnectionAnchor start) {
+		startAnchor = start;
+		updateStartPosition();
+	}
+	
+	/**
+	 * Set the endAnchor for this line.
+	 * After setting the EndPosition will be updated.
+	 * @param Anchor to end at.
+	 */
+	public void setEndAnchor(ConnectionAnchor end) {
+		endAnchor = end;
+		updateEndPosition();
+	}
+	
+	/**
+	 * Sets the start position for this Line object
+	 * @param x coordinate
+	 * @param y coordinate
+	 */
+	public void setStartPosition(double x, double y) {
+		setStartX(x);
+		setStartY(y);
+		setControlX1(x);
+		setControlY1(y+BEZIER_CONTROL_OFFSET_Y);
+	}
+	
+	/**
+	 * Sets the end position for this Line object.
+	 * @param x coordinate
+	 * @param y coordinate
+	 */
+	public void setEndPosition(double x, double y) {
+		setEndX(x);
+		setEndY(y);
+		setControlX2(x);
+		setControlY2(y-BEZIER_CONTROL_OFFSET_Y);
+	}
+	
+	/**
+	 * Runs both the update Start end End position functions.
+	 * Use when refreshing UI representation of the Line.
+	 */
+	protected void updateStartEndPositions() {
+		updateStartPosition();
+		updateEndPosition();
+	}
+	
+	/**
+	 * Refresh the Start position of this Line using
+	 * startAnchor as a reference point.
+	 */
+	private void updateStartPosition() {
+		setStartPosition(startAnchor.getLayoutX()+startAnchor.getCenterX()-this.getLayoutX(),startAnchor.getLayoutY()+startAnchor.getCenterY()-this.getLayoutY());
+	}
+	
+	/**
+	 * Refresh the End position of this Line using
+	 * endAnchor as a reference point.
+	 */
+	private void updateEndPosition() {
+		setEndPosition(endAnchor.getLayoutX()+endAnchor.getCenterX()-this.getLayoutX(),endAnchor.getLayoutY()+endAnchor.getCenterY()-this.getLayoutY());
+	}
+}
