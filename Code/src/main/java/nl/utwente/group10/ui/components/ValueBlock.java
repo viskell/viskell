978aac32a5af5132353ddf1e6677f72cfd73aef5
TheMSB <Martijn.bruning@gmail.com>
2015-03-26 16:38:12 +0100

    Created unit tests and fixed bugs in ValueBlock.java

--- Code/src/main/java/nl/utwente/group10/ui/components/ValueBlock.java
+++ Code/src/main/java/nl/utwente/group10/ui/components/ValueBlock.java
@@ -17,6 +17,9 @@ import java.io.IOException;
  */
 public class ValueBlock extends Block {
 	
+	/** The value of this ValueBlock.*/
+	private String blockValue;
+	
 	/**
 	 * Creates a new ValueBlock instance with initialized value,
 	 * once initialized value cannot be changed.
@@ -27,6 +30,24 @@ public class ValueBlock extends Block {
     public static ValueBlock newInstance(String value) throws IOException {
         ValueBlock block = FXMLLoader.load(ValueBlock.class.getResource("/ui/ValueBlock.fxml"), null, new TactileBuilderFactory());
         ((Label) block.lookup("#label_value")).setText(value);
+        block.setOutput(value);
+        
         return block;
+    }
+    
+    /**
+     * Sets the value of this block so it can be used as output.
+     * @param value
+     */
+    private void setOutput(String value) {
+    	blockValue = value;
+    }
+    
+    /**
+     * Returns the value that this block is outputting.
+     * @return output
+     */
+    public String getOutput() {
+    	return blockValue;
     }
 }
---
+++ Code/src/test/java/nl/utwente/group10/ui/componentTests/DisplayBlockTest.java
@@ -0,0 +1,56 @@
+package nl.utwente.group10.ui.componentTests;
+
+import static org.junit.Assert.*;
+import javafx.application.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import nl.utwente.group10.ui.components.DisplayBlock;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DisplayBlockTest {
+	
+	/**
+	 * Test Application extension with voided start method
+	 * to enable setup of unit testing.
+	 */
+	public static class JTestApp extends Application {
+	    @Override
+	    public void start(Stage primaryStage) throws Exception {
+	        //Do nothing
+	    }
+	}
+	
+	/**
+	 * Before testing start a JavaFX thread to be able to run
+	 * and test JavaFX elements
+	 */
+	@BeforeClass
+	public static void initJFX() {
+	    Thread t = new Thread("JavaFX Init Thread") {
+	        public void run() {
+	            Application.launch(JTestApp.class, new String[0]);
+	        }
+	    };
+	    t.setDaemon(true);
+	    t.start();
+	}
+
+	@Test
+	public void initTest() throws IOException {
+		assertNotNull(DisplayBlock.newInstance());
+	}
+	
+	@Test
+	public void inputOutputTest() throws IOException {
+		DisplayBlock block = DisplayBlock.newInstance();
+		assertNull(block.getOutput());
+		
+		block.setInput("8");
+		assertEquals(block.getOutput(), "8");
+	}
+}
