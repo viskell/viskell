234af560ac6215b58e222f33b88ae827c59e1c32
Jan-Jelle Kester <janjelle@jjkester.nl>
2015-03-26 17:09:06 +0100

    Add logger for haskell.type in Type

--- Code/src/main/java/nl/utwente/group10/haskell/type/Type.java
+++ Code/src/main/java/nl/utwente/group10/haskell/type/Type.java
@@ -2,6 +2,8 @@ package nl.utwente.group10.haskell.type;
 
 import nl.utwente.group10.haskell.HaskellObject;
 
+import java.util.logging.Logger;
+
 /**
  * Abstract class for Haskell types. Provides an interface for common methods.
  *
@@ -9,6 +11,8 @@ import nl.utwente.group10.haskell.HaskellObject;
  * the same class should be equal, that is {@code this.equals(that)} should return {@code true}.
  */
 public abstract class Type extends HaskellObject {
+    /** Logger instance for types. */
+    protected Logger logger = Logger.getLogger(Type.class.getName());
 
     /**
      * Prunes the type subtree and returns the resolved type for this type. The resolved type can be this type itself.
