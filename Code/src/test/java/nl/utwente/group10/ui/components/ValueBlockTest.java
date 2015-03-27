74ec011f6451c6389d3731f31e2c604b4992298c
TheMSB <Martijn.bruning@gmail.com>
2015-03-27 22:01:07 +0100

    fixed unit tests

--- Code/src/test/java/nl/utwente/group10/ui/components/ValueBlockTest.java
+++ Code/src/test/java/nl/utwente/group10/ui/components/ValueBlockTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class ValueBlockTest {
+public class ValueBlockTest extends ComponentTest {
     @Test
     public void initTest() throws IOException {
         assertNotNull(ValueBlock.newInstance("6"));
@@ -16,6 +16,6 @@ public class ValueBlockTest {
     @Test
     public void outputTest() throws IOException {
         ValueBlock block = ValueBlock.newInstance("6");
-        assertEquals(block.getOutput(), "6");
+        assertEquals(block.getValue(), "6");
     }
 }
