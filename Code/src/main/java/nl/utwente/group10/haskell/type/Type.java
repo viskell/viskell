097bdd7f016ec7c0e446bd856505ee2d7c47a067
Jan-Jelle Kester <janjelle@jjkester.nl>
2015-03-27 11:39:36 +0100

    Extend prune documentation

--- Code/src/main/java/nl/utwente/group10/haskell/type/Type.java
+++ Code/src/main/java/nl/utwente/group10/haskell/type/Type.java
@@ -12,7 +12,17 @@ public abstract class Type extends HaskellObject {
     protected Logger logger = Logger.getLogger(Type.class.getName());
 
     /**
-     * Prunes the type subtree and returns the resolved type for this type. The resolved type can be this type itself.
+     * Prunes the type subtree and returns the resolved type for this type. The inferred type can be this type itself.
+     *
+     * During the unify progress of the Hindley-Milner algorithm, a type might be unified with another type. This means
+     * that for some types a more concrete type can be used instead. For example, a variable type 'a' could be unified
+     * with a constant type 'Int', in which case you could use 'Int' in all places where 'a' is used (and means the
+     * same). Pruning makes sure that you are using a concrete type where possible and can be used to provide a better
+     * output to end users.
+     *
+     * Pruning is not needed to provide correct type inference, but can be used without losing correct inference in the
+     * future.
+     *
      * @return The pruned type of this type.
      */
     public abstract Type prune();
