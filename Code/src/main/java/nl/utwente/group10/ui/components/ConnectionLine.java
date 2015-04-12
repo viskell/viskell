6b12311057dba95f8c17305a13958e2cc993b1cd
i3anaan <i3anaan@live.nl>
2015-04-12 16:44:53 +0200

    Made the ConnectionLine curviness (bezier offset) dependent on the current start and end positions.

--- Code/src/main/java/nl/utwente/group10/ui/components/ConnectionLine.java
+++ Code/src/main/java/nl/utwente/group10/ui/components/ConnectionLine.java
@@ -17,9 +17,15 @@ import java.io.IOException;
 public class ConnectionLine extends CubicCurve {
 	/**
 	 * Control offset for this bezier of this line. in simple terms: controls
-	 * the curviness of the line
+	 * the curviness of the line.
+	 * 
+	 * A higher offset equates to a more curved line. The offset is a function
+	 * of the offset multiplier and the straight line length. The offset will
+	 * always be between the defined minimum and maximum.
 	 */
-	public static final double BEZIER_CONTROL_OFFSET_Y = 100f;
+	public static final double BEZIER_CONTROL_OFFSET_MULTIPLIER = 0.7f;
+	public static final double BEZIER_CONTROL_OFFSET_MINIMUM = 10f;
+	public static final double BEZIER_CONTROL_OFFSET_MAXIMUM = 200f;
 
 	/** The fxmlLoader responsible for loading the fxml. */
 	private FXMLLoader fxmlLoader;
@@ -44,14 +50,15 @@ public class ConnectionLine extends CubicCurve {
 	/**
 	 * Sets the start position for this Line object
 	 *
-	 * @param x coordinate
-	 * @param y coordinate
+	 * @param x
+	 *            coordinate
+	 * @param y
+	 *            coordinate
 	 */
 	public void setStartPosition(double x, double y) {
 		setStartX(x);
 		setStartY(y);
-		setControlX1(x);
-		setControlY1(y + BEZIER_CONTROL_OFFSET_Y);
+		setBezierOffset();
 	}
 
 	public void setStartPosition(Point2D point) {
@@ -61,17 +68,54 @@ public class ConnectionLine extends CubicCurve {
 	/**
 	 * Sets the end position for this Line object.
 	 *
-	 * @param x coordinate
-	 * @param y coordinate
+	 * @param x
+	 *            coordinate
+	 * @param y
+	 *            coordinate
 	 */
 	public void setEndPosition(double x, double y) {
 		setEndX(x);
 		setEndY(y);
-		setControlX2(x);
-		setControlY2(y - BEZIER_CONTROL_OFFSET_Y);
+		setBezierOffset();
 	}
 
 	public void setEndPosition(Point2D point) {
 		setEndPosition(point.getX(), point.getY());
+	}
+
+	/**
+	 * Updates the Bezier offset (curviness) according to the current start and end positions.
+	 */
+	public void setBezierOffset() {
+		setControlX1(getStartX());
+		setControlY1(getStartY() + getCurrentBezierOffset());
+		setControlX2(getEndX());
+		setControlY2(getEndY() - getCurrentBezierOffset());
+	}
+
+	/**
+	 * @return The current bezier offset based on the current start and end positions.
+	 */
+	public double getCurrentBezierOffset() {
+		double unclamped = BEZIER_CONTROL_OFFSET_MULTIPLIER
+				* getStraightLineLength();
+		return Math.max(Math.min(unclamped, BEZIER_CONTROL_OFFSET_MAXIMUM),
+				BEZIER_CONTROL_OFFSET_MINIMUM);
+	}
+
+	/**
+	 * @return The length of a direct line between the end and start point of
+	 *         this CubicCurve
+	 */
+	public double getStraightLineLength() {
+		return Math.sqrt(getDeltaX() * getDeltaX() + getDeltaY() * getDeltaY());
+	}
+
+	public double getDeltaX() {
+		return this.getEndX() - this.getStartX();
+	}
+
+	public double getDeltaY() {
+		return this.getEndY() - this.getStartY();
 	}
 }
