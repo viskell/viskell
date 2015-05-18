18af0945af8528dada67a60fa10be8dc5945dbd4
Wander Nauta <info@wandernauta.nl>
2015-05-18 17:53:36 +0200

    Make GraphBlock calculate and show actual results

--- Code/src/main/java/nl/utwente/group10/ui/components/blocks/GraphBlock.java
+++ Code/src/main/java/nl/utwente/group10/ui/components/blocks/GraphBlock.java
@@ -1,20 +1,27 @@
 package nl.utwente.group10.ui.components.blocks;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
+import nl.utwente.group10.haskell.exceptions.HaskellException;
+import nl.utwente.group10.haskell.expr.Apply;
 import nl.utwente.group10.haskell.expr.Expr;
+import nl.utwente.group10.haskell.expr.Ident;
 import nl.utwente.group10.haskell.type.ConstT;
 import nl.utwente.group10.haskell.type.FuncT;
 import nl.utwente.group10.haskell.type.Type;
 import nl.utwente.group10.ui.CustomUIPane;
 import nl.utwente.group10.ui.components.anchors.InputAnchor;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class GraphBlock extends Block implements InputBlock {
     private InputAnchor input;
@@ -24,6 +31,12 @@ public class GraphBlock extends Block implements InputBlock {
 
     @FXML
     private LineChart<Double, Double> chart;
+
+    @FXML
+    private NumberAxis x;
+
+    @FXML
+    private NumberAxis y;
 
     public GraphBlock(CustomUIPane pane) {
         super(pane);
@@ -75,14 +88,47 @@ public class GraphBlock extends Block implements InputBlock {
     public void invalidateConnectionState() {
         ObservableList<XYChart.Series<Double, Double>> lineChartData = FXCollections.observableArrayList();
 
-        LineChart.Series<Double, Double> series1 = new LineChart.Series<>();
-        series1.getData().add(new XYChart.Data<Double, Double>(0.0, 1.0));
-        series1.getData().add(new XYChart.Data<Double, Double>(1.2, 1.4));
-        series1.getData().add(new XYChart.Data<Double, Double>(2.2, 1.9));
-        series1.getData().add(new XYChart.Data<Double, Double>(2.7, 2.3));
-        series1.getData().add(new XYChart.Data<Double, Double>(2.9, 0.5));
+        double step = 0.01;
+        double min = x.getLowerBound();
+        double max = x.getUpperBound();
 
-        lineChartData.add(series1);
+        // Haskell equivalent:
+        // putStrLn $ unwords $ map show $ map (id) [1.0,1.1..5.0]
+        Expr expr = new Apply(
+            new Ident("putStrLn"),
+            new Apply(
+                new Ident("unwords"),
+                new Apply(
+                    new Apply(
+                        new Ident("map"),
+                        new Ident("show")
+                    ),
+                    new Apply(
+                        new Apply(
+                            new Ident("map"),
+                            asExpr()
+                        ),
+                        new Ident(String.format("[%f,%f..%f]", min, min+step, max))
+                    )
+                )
+            )
+        );
+
+        try {
+            String results = getPane().getGhciSession().get().pull(expr);
+
+            LineChart.Series<Double, Double> series = new LineChart.Series<>();
+            ObservableList<XYChart.Data<Double, Double>> data = series.getData();
+            Iterator<String> v = Splitter.on(' ').split(results).iterator();
+
+            for (double i = min; i < max; i += step) {
+                data.add(new XYChart.Data<>(i, Double.valueOf(v.next())));
+            }
+
+            lineChartData.add(series);
+        } catch (HaskellException | NoSuchElementException | NumberFormatException ignored) {
+            // Pretend we didn't hear anything.
+        }
 
         chart.setData(lineChartData);
     }
