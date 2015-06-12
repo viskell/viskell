7eae58e77652a5e2db6d754959114e5c07a66629
Wander Nauta <info@wandernauta.nl>
2015-06-12 21:05:09 +0200

    Add enough bits and pieces to make it 'work'

--- Code/src/main/java/nl/utwente/group10/ui/components/blocks/DefinitionBlock.java
+++ Code/src/main/java/nl/utwente/group10/ui/components/blocks/DefinitionBlock.java
@@ -1,9 +1,11 @@
 package nl.utwente.group10.ui.components.blocks;
 
+import com.google.common.collect.ImmutableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import nl.utwente.group10.haskell.env.Env;
 import nl.utwente.group10.haskell.expr.*;
+import nl.utwente.group10.haskell.hindley.HindleyMilner;
 import nl.utwente.group10.haskell.type.ConstT;
 import nl.utwente.group10.haskell.type.FuncT;
 import nl.utwente.group10.haskell.type.Type;
@@ -12,10 +14,11 @@ import nl.utwente.group10.ui.CustomUIPane;
 import nl.utwente.group10.ui.components.ComponentLoader;
 import nl.utwente.group10.ui.components.anchors.InputAnchor;
 import nl.utwente.group10.ui.components.anchors.OutputAnchor;
+import org.codehaus.plexus.util.cli.Commandline;
 
 import java.util.List;
 
-public class DefinitionBlock extends Block implements OutputBlock, ComponentLoader {
+public class DefinitionBlock extends Block implements InputBlock, OutputBlock, ComponentLoader {
     @Override
     public OutputAnchor getOutputAnchor() {
         return fun;
@@ -43,17 +46,81 @@ public class DefinitionBlock extends Block implements OutputBlock, ComponentLoad
 
     private Type type;
 
-    private class ArgumentBlock extends Block {
-        private Expr arg;
+    @Override
+    public Type getInputSignature(InputAnchor input) {
+        return HindleyMilner.makeVariable();
+    }
 
-        public ArgumentBlock(CustomUIPane pane, Expr arg) {
-            super(pane);
-            this.arg = arg;
+    @Override
+    public Type getInputSignature(int index) {
+        return HindleyMilner.makeVariable();
+    }
+
+    @Override
+    public Type getInputType(InputAnchor input) {
+        return HindleyMilner.makeVariable();
+    }
+
+    @Override
+    public Type getInputType(int index) {
+        return HindleyMilner.makeVariable();
+    }
+
+    @Override
+    public List<InputAnchor> getAllInputs() {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public List<InputAnchor> getActiveInputs() {
+        return ImmutableList.of();
+    }
+
+    private class ArgumentBlock extends Block implements OutputBlock {
+        private Type type;
+        private Function.FunctionArgument arg;
+        private OutputAnchor anchor;
+
+        public ArgumentBlock(DefinitionBlock parent, Type type) {
+            super(parent.getPane());
+            this.type = type;
+            this.arg = new Function.FunctionArgument(type);
+
+            anchor = new OutputAnchor(this, parent.getPane());
         }
 
         @Override
         public Expr asExpr() {
             return arg;
+        }
+
+        public Function.FunctionArgument getArgument() {
+            return arg;
+        }
+
+        @Override
+        public OutputAnchor getOutputAnchor() {
+            return anchor;
+        }
+
+        @Override
+        public Type getOutputType() {
+            return type;
+        }
+
+        @Override
+        public Type getOutputType(Env env) {
+            return type;
+        }
+
+        @Override
+        public Type getOutputSignature() {
+            return type;
+        }
+
+        @Override
+        public Type getOutputSignature(Env env) {
+            return type;
         }
     }
     /** The Anchor that is used as input. */
@@ -62,6 +129,9 @@ public class DefinitionBlock extends Block implements OutputBlock, ComponentLoad
     @FXML private Pane argSpace;
     @FXML private Pane resSpace;
     @FXML private Pane funSpace;
+
+    private ArgumentBlock argBlock;
+    private OutputAnchor arg;
 
     private InputAnchor res;
     private OutputAnchor fun;
@@ -72,7 +142,8 @@ public class DefinitionBlock extends Block implements OutputBlock, ComponentLoad
 
         type = new FuncT(new ConstT("Float"), new ConstT("Float"));
 
-        OutputAnchor arg = new OutputAnchor(new ArgumentBlock(pane, new Function.FunctionArgument(new VarT("a"))), pane);
+        argBlock = new ArgumentBlock(this, new ConstT("Float"));
+        arg = argBlock.getOutputAnchor();
         argSpace.getChildren().add(arg);
 
         res = new InputAnchor(this, pane);
@@ -84,10 +155,6 @@ public class DefinitionBlock extends Block implements OutputBlock, ComponentLoad
 
     @Override
     public Expr asExpr() {
-        Function.FunctionArgument arg = new Function.FunctionArgument(new ConstT("Float"));
-
-        Expr body = new Apply(new Ident("sin"), arg);
-
-        return new Function(body, arg);
+        return new Function(res.asExpr(), argBlock.getArgument());
     }
 }
