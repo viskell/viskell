86dcd436cd57805e9064ad313c26535c8dea911c
Wander Nauta <info@wandernauta.nl>
2015-10-13 15:36:34 +0200

    Add File menu actions to global menu

--- Code/src/main/java/nl/utwente/group10/ui/components/menu/GlobalMenu.java
+++ Code/src/main/java/nl/utwente/group10/ui/components/menu/GlobalMenu.java
@@ -1,19 +1,69 @@
 package nl.utwente.group10.ui.components.menu;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import nl.utwente.group10.ui.CustomUIPane;
+
+import java.io.File;
 
 /**
  * A context menu with global actions (i.e. quit).
  */
 public class GlobalMenu extends ContextMenu {
-    public GlobalMenu() {
+    private CustomUIPane pane;
+
+    public GlobalMenu(CustomUIPane pane) {
         super();
+        this.pane = pane;
+
+        MenuItem menuNew = new MenuItem("New");
+        menuNew.setOnAction(this::onNew);
+
+        MenuItem menuOpen = new MenuItem("Open");
+        menuOpen.setOnAction(this::onOpen);
+
+        MenuItem menuSave = new MenuItem("Save");
+        menuSave.setOnAction(this::onSave);
+
+        MenuItem menuSaveAs = new MenuItem("Save as");
+        menuSaveAs.setOnAction(this::onSaveAs);
 
         MenuItem menuQuit = new MenuItem("Quit");
-        menuQuit.setOnAction(e -> Platform.exit());
+        menuQuit.setOnAction(this::onQuit);
 
-        this.getItems().addAll(menuQuit);
+        this.getItems().addAll(menuNew, menuOpen, menuSave, menuSaveAs, menuQuit);
+    }
+
+    private void onNew(ActionEvent actionEvent) {
+        pane.getChildren().clear();
+    }
+
+    private void onOpen(ActionEvent actionEvent) {
+        Window window = pane.getScene().getWindow();
+        File file = new FileChooser().showOpenDialog(window);
+
+        if (file != null) {
+            /* Load file... */
+        }
+    }
+
+    private void onSave(ActionEvent actionEvent) {
+    }
+
+    private void onSaveAs(ActionEvent actionEvent) {
+        Window window = pane.getScene().getWindow();
+        File file = new FileChooser().showSaveDialog(window);
+
+        if (file != null) {
+            /* Save file... */
+        }
+    }
+
+    private void onQuit(ActionEvent actionEvent) {
+        Platform.exit();
     }
 }
