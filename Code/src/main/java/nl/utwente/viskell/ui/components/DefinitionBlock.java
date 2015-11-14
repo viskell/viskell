c5b86e1878a9bad4ffc2451e99a720c0c8ca00ab
Arjan <w.a.boeijink@utwente.nl>
2015-11-14 11:09:35 +0100

    Split Defintion block into a inner and outer part
    Also types now only propagate outwards from a lambda.

--- Code/src/main/java/nl/utwente/viskell/ui/components/DefinitionBlock.java
+++ Code/src/main/java/nl/utwente/viskell/ui/components/DefinitionBlock.java
@@ -3,22 +3,16 @@ package nl.utwente.viskell.ui.components;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import nl.utwente.ewi.caes.tactilefx.control.TactilePane;
-import nl.utwente.viskell.haskell.expr.Annotated;
 import nl.utwente.viskell.haskell.expr.Binder;
 import nl.utwente.viskell.haskell.expr.Expression;
-import nl.utwente.viskell.haskell.expr.Lambda;
-import nl.utwente.viskell.haskell.expr.LetExpression;
-import nl.utwente.viskell.haskell.type.FunType;
 import nl.utwente.viskell.haskell.type.Type;
-import nl.utwente.viskell.haskell.type.TypeScope;
 import nl.utwente.viskell.ui.ComponentLoader;
 import nl.utwente.viskell.ui.CustomUIPane;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableList;
 
@@ -27,61 +21,12 @@ import com.google.common.collect.ImmutableList;
  */
 public class DefinitionBlock extends Block implements ComponentLoader {
 
-    // TODO make this an independent class if other blocks (case?) need this too 
-    /** An internal output anchor for an argument binder. */
-    public static class BinderAnchor extends OutputAnchor {
-
-        public BinderAnchor(DefinitionBlock parent, Binder binder) {
-            super(parent, binder);
-        }
-
-        @Override
-        protected void extendExprGraph(LetExpression exprGraph) {
-            return; // the scope of graph is limited its parent
-        }
-    }
-
-    // TODO make this an independent class if other blocks (case?) need this too
-    /** An internal input anchor for a local result. */
-    public static class ResultAnchor extends InputAnchor {
-        /** The optional type of the result of the function (the last part of the signature). */
-        private final Optional<Type> resType;
-
-        public ResultAnchor(DefinitionBlock parent, Optional<Type> resType) {
-            super(parent);
-            this.resType = resType;
-        }
-        
-        @Override
-        public Expression getLocalExpr() {
-            if (this.resType.isPresent()) {
-                return new Annotated(super.getLocalExpr(), this.resType.get());
-            }
-           
-            return super.getLocalExpr();
-        }
-        
-        /** Set fresh type for the next typechecking cycle.*/
-        private void refreshAnchorType(TypeScope scope) {
-            if (this.resType.isPresent()) {
-                this.setFreshRequiredType(this.resType.get(), scope);
-            } else {
-                this.setFreshRequiredType(TypeScope.unique("y"), scope);
-            }
-        }
-    }
-    
-    @FXML private Pane argSpace;
-    @FXML private Pane resSpace;
     @FXML private Pane funSpace;
 
     @FXML private Label signature;
 
-    private List<BinderAnchor> args;
-
-    /** The result anchor (first bottom anchor) */
-    private ResultAnchor res;
-
+    private LambdaContainer body;
+    
     /** The function anchor (second bottom anchor) */
     private OutputAnchor fun;
 
@@ -96,37 +41,30 @@ public class DefinitionBlock extends Block implements ComponentLoader {
 
         this.signature.setText("");
         this.signature.setVisible(false);
-        this.args = new ArrayList<>();
-        for (int i = 0; i < arity; i++) {
-            this.args.add(new BinderAnchor(this, new Binder("x_" + i)));
-        }
-        this.res = new ResultAnchor(this, Optional.empty());
-        this.setupAnchors();
+        
+        this.body = new LambdaContainer(this, arity);
+        ((VBox)this.getChildren().get(0)).getChildren().add(1, this.body);
+        
+        this.fun = new OutputAnchor(this, new Binder("lam"));
+        this.funSpace.getChildren().add(this.fun);
+        TactilePane.setGoToForegroundOnContact(this, false);
     }
             
+    /**
+     * Constructs a DefinitionBlock with an explicitly type function.
+     * @param pane the parent ui pane.
+     * @param name of the function.
+     * @param type the full function type.
+     */
     public DefinitionBlock(CustomUIPane pane, String name, Type type) {
         super(pane);
         this.loadFXML("DefinitionBlock");
 
-        this.args = new ArrayList<>();
         this.signature.setText(name + " :: " + type.prettyPrint());
-        // Collect argument types and result type
-        Type t = type;
-        int i = 0;
-        while (t instanceof FunType) {
-            FunType ft = (FunType) t;
-            args.add(new BinderAnchor(this, new Binder(name + "_x" + i, ft.getArgument())));
-            i++;
-            t = ft.getResult();
-        }
-        this.res = new ResultAnchor(this, Optional.of(t));
 
-        this.setupAnchors();
-    }
-
-    private void setupAnchors() {
-        this.argSpace.getChildren().addAll(this.args);
-        this.resSpace.getChildren().add(this.res);
+        this.body = new LambdaContainer(this, name, type);
+        ((VBox)this.getChildren().get(0)).getChildren().add(1, this.body);
+        
         this.fun = new OutputAnchor(this, new Binder("lam"));
         this.funSpace.getChildren().add(this.fun);
         TactilePane.setGoToForegroundOnContact(this, false);
@@ -144,32 +82,16 @@ public class DefinitionBlock extends Block implements ComponentLoader {
 
     @Override
     public void refreshAnchorTypes() {
-        TypeScope scope = new TypeScope();
-        for (BinderAnchor arg : this.args) {
-            arg.refreshType(scope);
-        }
-        this.res.refreshAnchorType(scope);
-        
-        ArrayList<Type> types = new ArrayList<>();
-        for (BinderAnchor arg : this.args) {
-            types.add(arg.getType());
-        }
-        types.add(this.res.getType());
+        // do typechecking internal connections first so that the lambda type is inferred
+        this.body.handleConnectionChanges(false);
 
-        this.fun.setExactRequiredType(Type.fun(types.toArray(new Type[this.args.size()+1])));
+        this.fun.setExactRequiredType(this.body.getLambdaType().getFresh());
     }
 
     @Override
     protected void propagateConnectionChanges(boolean finalPhase) {
-        // first propagate into the internal blocks
-        this.res.getConnection().ifPresent(c -> c.handleConnectionChangesUpwards(finalPhase));
-
-        // also propagate in from above in case the lambda is partially connected 
-        for (BinderAnchor arg : this.args) {
-            for (InputAnchor anchor : arg.getOppositeAnchors()) {
-                anchor.handleConnectionChanges(finalPhase);
-            }
-        }
+        // first propagate into the internals
+        this.body.handleConnectionChanges(finalPhase);
 
         // continue as normal with propagating changes on the outside
         super.propagateConnectionChanges(finalPhase);
@@ -177,16 +99,13 @@ public class DefinitionBlock extends Block implements ComponentLoader {
     
     @Override
     public final Expression getLocalExpr() {
-        List<Binder> binders = this.args.stream().map(arg -> arg.binder).collect(Collectors.toList());
-        LetExpression body = new LetExpression(this.res.getLocalExpr());
-        this.res.extendExprGraph(body);
-        return new Lambda(binders, body);
+        return this.body.getLocalExpr();
     }
 
     @Override
     public void invalidateVisualState() {
-        // also update the internal blocks connected to the internal anchor 
-        this.res.getOppositeAnchor().ifPresent(a -> a.invalidateVisualState());
+        this.body.invalidateVisualState();
+        // TODO update fun anchor when it gets a type label
     }
 
 }
---
+++ Code/src/main/java/nl/utwente/viskell/ui/components/LambdaContainer.java
@@ -0,0 +1,233 @@
+package nl.utwente.viskell.ui.components;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import nl.utwente.viskell.haskell.expr.Annotated;
+import nl.utwente.viskell.haskell.expr.Binder;
+import nl.utwente.viskell.haskell.expr.Expression;
+import nl.utwente.viskell.haskell.expr.Lambda;
+import nl.utwente.viskell.haskell.expr.LetExpression;
+import nl.utwente.viskell.haskell.type.FunType;
+import nl.utwente.viskell.haskell.type.Type;
+import nl.utwente.viskell.haskell.type.TypeScope;
+import nl.utwente.viskell.ui.ComponentLoader;
+
+public class LambdaContainer extends BorderPane implements ComponentLoader {
+
+    // TODO make this an independent class if other blocks (case?) need this too 
+    /** An internal output anchor for an argument binder. */
+    public class BinderAnchor extends OutputAnchor {
+
+        // FIXME BinderAnchor should not have or use the DefinitionBlock parent
+        public BinderAnchor(DefinitionBlock parent, Binder binder) {
+            super(parent, binder);
+        }
+
+        @Override
+        protected void extendExprGraph(LetExpression exprGraph) {
+            return; // the scope of graph is limited its parent
+        }
+        
+        @Override
+        public void initiateConnectionChanges() {
+            // Starts a new (2 phase) change propagation process from this lambda.
+            LambdaContainer.this.handleConnectionChanges(false);
+            LambdaContainer.this.handleConnectionChanges(true);
+        }
+
+        @Override
+        public void prepareConnectionChanges() {
+            LambdaContainer.this.refreshAnchorTypes();
+        }
+
+        @Override
+        protected void handleConnectionChanges(boolean finalPhase) {
+            LambdaContainer.this.handleConnectionChanges(finalPhase);
+        }
+    }
+
+    // TODO make this an independent class if other blocks (case?) need this too
+    /** An internal input anchor for a local result. */
+    public class ResultAnchor extends InputAnchor {
+        /** The optional type of the result of the function (the last part of the signature). */
+        private final Optional<Type> resType;
+        
+        // FIXME ResultAnchor should not have or use the DefinitionBlock parent
+        public ResultAnchor(DefinitionBlock parent, Optional<Type> resType) {
+            super(parent);
+            this.resType = resType;
+        }
+        
+        @Override
+        public Expression getLocalExpr() {
+            if (this.resType.isPresent()) {
+                return new Annotated(super.getLocalExpr(), this.resType.get());
+            }
+           
+            return super.getLocalExpr();
+        }
+        
+        /** Set fresh type for the next typechecking cycle.*/
+        private void refreshAnchorType(TypeScope scope) {
+            if (this.resType.isPresent()) {
+                this.setFreshRequiredType(this.resType.get(), scope);
+            } else {
+                this.setFreshRequiredType(TypeScope.unique("y"), scope);
+            }
+        }
+
+        @Override
+        protected void handleConnectionChanges(boolean finalPhase) {
+            LambdaContainer.this.handleConnectionChanges(finalPhase);
+        }
+    }
+    
+    /* area containing argument anchors */
+    @FXML private Pane argSpace;
+    
+    /* area contain result anchor */
+    @FXML private Pane resSpace;
+
+    /* The */
+    private DefinitionBlock wrapper;
+    
+    /* The lambda argument anchors */
+    private List<BinderAnchor> args;
+
+    /** The result anchor */
+    private ResultAnchor res;
+
+    /** Whether the anchor types are fresh*/
+    private boolean freshAnchorTypes;
+    
+    /** Status of change updating process in this block. */
+    private boolean updateInProgress;
+
+    /**
+     * Constructs a LambdaContainer for an untyped lambda of n arguments.
+     * @param arity the number of arguments of this lambda.
+     */
+    public LambdaContainer(DefinitionBlock wrapper, int arity) {
+        super();
+        this.loadFXML("LambdaContainer");
+        this.wrapper = wrapper;
+        
+        this.args = new ArrayList<>();
+        for (int i = 0; i < arity; i++) {
+            this.args.add(this.new BinderAnchor(wrapper, new Binder("x_" + i)));
+        }
+        this.res = this.new ResultAnchor(wrapper, Optional.empty());
+        
+        this.argSpace.getChildren().addAll(this.args);
+        this.resSpace.getChildren().add(this.res);
+    }
+    
+    /**
+     * Constructs a LambdaContainer with explicit types.
+     * @param name of the function.
+     * @param type the full function type.
+     */
+    public LambdaContainer(DefinitionBlock wrapper, String name, Type type) {
+        super();
+        this.loadFXML("LambdaContainer");
+        this.wrapper = wrapper;
+
+        // Collect argument types and result type
+        this.args = new ArrayList<>();
+        Type t = type;
+        int i = 0;
+        while (t instanceof FunType) {
+            FunType ft = (FunType) t;
+            args.add(this.new BinderAnchor(wrapper, new Binder(name + "_x" + i, ft.getArgument())));
+            i++;
+            t = ft.getResult();
+        }
+        this.res = this.new ResultAnchor(wrapper, Optional.of(t));
+
+        this.argSpace.getChildren().addAll(this.args);
+        this.resSpace.getChildren().add(this.res);
+    }
+
+    /** Set fresh types in all anchors of this lambda for the next typechecking cycle. */
+    protected void refreshAnchorTypes() {
+        if (this.updateInProgress || this.freshAnchorTypes) {
+            return; // refresh anchor types only once
+        }
+        this.freshAnchorTypes = true;
+        
+        TypeScope scope = new TypeScope();
+        for (BinderAnchor arg : this.args) {
+            arg.refreshType(scope);
+        }
+        this.res.refreshAnchorType(scope);
+    }
+    
+    /** returns the current (inferred) type of this lambda */
+    protected Type getLambdaType() {
+        ArrayList<Type> types = new ArrayList<>();
+        for (BinderAnchor arg : this.args) {
+            types.add(arg.getType());
+        }
+        types.add(this.res.getType());
+
+        return Type.fun(types.toArray(new Type[this.args.size()+1]));
+    }
+    
+    /**
+     * Handle the expression and types changes caused by modified connections or values.
+     * @param finalPhase whether the change propagation is in the second (final) phase.
+     */
+    public final void handleConnectionChanges(boolean finalPhase) {
+        if (! finalPhase) {
+            this.refreshAnchorTypes();
+        }
+        this.propagateConnectionChanges(finalPhase);
+    }
+    
+    /**
+     * Propagate the changes through connected blocks, then trigger a visual update.
+     * @param finalPhase whether the change propagation is in the second (final) phase.
+     */
+    private void propagateConnectionChanges(boolean finalPhase) {
+        if (this.updateInProgress != finalPhase) {
+            return; // avoid doing extra work and infinite recursion
+        }
+        this.updateInProgress = !finalPhase;
+        this.freshAnchorTypes = false;
+        
+        // first propagate up from the result anchor
+        this.res.getConnection().ifPresent(c -> c.handleConnectionChangesUpwards(finalPhase));
+
+        // also propagate in from above in case the lambda is partially connected 
+        for (BinderAnchor arg : this.args) {
+            for (InputAnchor anchor : arg.getOppositeAnchors()) {
+                anchor.handleConnectionChanges(finalPhase);
+                // take the type of argument connections in account even if the connected block is being processed
+                anchor.getConnection().ifPresent(c -> c.handleConnectionChangesUpwards(finalPhase));
+            }
+        }
+
+        // propagate internal type changes outwards
+        this.wrapper.handleConnectionChanges(finalPhase);
+    }
+    
+    /** @return The local expression this LambdaContainer represents. */
+    public Expression getLocalExpr() {
+        List<Binder> binders = this.args.stream().map(arg -> arg.binder).collect(Collectors.toList());
+        LetExpression body = new LetExpression(this.res.getLocalExpr());
+        this.res.extendExprGraph(body);
+        return new Lambda(binders, body);
+    }
+
+    /** Called when the VisualState changed. */
+    public void invalidateVisualState() {
+        // TODO update anchors when they get a type label       
+    }
+    
+}
