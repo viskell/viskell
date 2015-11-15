b5dbe12153c1043abca37afa7c68e29ec5427e4a
Wander Nauta <info@wandernauta.nl>
2015-11-15 13:19:09 +0100

    Give every Box a position

--- Code/src/main/java/nl/utwente/viskell/model/Box.java
+++ Code/src/main/java/nl/utwente/viskell/model/Box.java
@@ -3,6 +3,11 @@ package nl.utwente.viskell.model;
 import com.google.common.collect.ImmutableList;
 
 public abstract class Box implements ModelElement {
+    /** X position of this Box in some unit */
+    private float x;
+
+    /** Y position of this Box in some unit */
+    private float y;
     
     public abstract BoxGroup getDirectParent();
     
@@ -22,4 +27,19 @@ public abstract class Box implements ModelElement {
     /** Updates the expression */
     protected abstract void updateExpr();
 
+    /** @return the X coordinate of this Box's top-left corner. */
+    public float getX() {
+        return x;
+    }
+
+    /** @return the Y coordinate of this Box's top-left corner. */
+    public float getY() {
+        return y;
+    }
+
+    /** Set the position of this Box's top-left corner. */
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
