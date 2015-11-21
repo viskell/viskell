35f6c3a50e043219038fd94f342d1d69ff1980f5
Arjan <w.a.boeijink@utwente.nl>
2015-11-21 18:13:14 +0100

    Simplify debugparent and touchdisplay

--- Code/src/main/java/nl/utwente/ewi/caes/tactilefx/DebugParent.java
+++ Code/src/main/java/nl/utwente/ewi/caes/tactilefx/DebugParent.java
@@ -1,7 +1,5 @@
 package nl.utwente.ewi.caes.tactilefx;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.ScaleTransition;
 import javafx.beans.Observable;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
@@ -11,8 +9,6 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.util.Duration;
 
 import java.util.Map;
 import java.util.TreeMap;
@@ -21,14 +17,9 @@ public class DebugParent extends StackPane {
     
     Pane overlay = new Pane();
     Map<Integer, TouchDisplay> circleByTouchId = new TreeMap<>();
-    Map<Integer, Line> lineByTouchId = new TreeMap<>();
-
-    int touchSetId = 0;
-    boolean active = false;
 
     public DebugParent(Node node) {
         super(node);
-        final double touchCircleRadius = 50.0;
 
         overlay.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
 
@@ -49,28 +40,10 @@ public class DebugParent extends StackPane {
             double x = event.getTouchPoint().getSceneX();
             double y = event.getTouchPoint().getSceneY();
 
-            TouchDisplay circle = new TouchDisplay(x, y, touchCircleRadius, touchId);
+            TouchDisplay circle = new TouchDisplay(x, y, bounds, touchId);
             circleByTouchId.put(touchId, circle);
             overlay.getChildren().add(circle);
-
-            Line line = new Line(x, y, bounds.getMinX(), bounds.getMinY());
-            lineByTouchId.put(touchId, line);
-            overlay.getChildren().add(line);
-
             circle.relocate(x, y);
-
-            ScaleTransition st = new ScaleTransition(new Duration(200), circle);
-            st.setFromX(0);
-            st.setFromY(0);
-            st.setToX(1);
-            st.setToY(1);
-
-            FadeTransition ft = new FadeTransition(new Duration(200), line);
-            ft.setFromValue(0);
-            ft.setToValue(1);
-
-            st.play();
-            ft.play();
         });
 
         addEventFilter(TouchEvent.TOUCH_MOVED, event -> {
@@ -82,39 +55,13 @@ public class DebugParent extends StackPane {
             double y = event.getTouchPoint().getY();
 
             TouchDisplay circle = circleByTouchId.get(touchId);
-            circle.relocate(x, y);
-
-            Line line = lineByTouchId.get(touchId);
-            line.setStartX(x);
-            line.setStartY(y);
-            line.setEndX(bounds.getMinX());
-            line.setEndY(bounds.getMinY());
+            circle.relocate(x, y, bounds);
         });
 
         addEventFilter(TouchEvent.TOUCH_RELEASED, event -> {
             int touchId = event.getTouchPoint().getId();
-
             TouchDisplay circle = circleByTouchId.get(touchId);
-            Line line = lineByTouchId.get(touchId);
-
-            ScaleTransition st = new ScaleTransition(new Duration(200), circle);
-            st.setFromX(1);
-            st.setFromY(1);
-            st.setToX(0);
-            st.setToY(0);
-            st.setOnFinished(e -> {
-                overlay.getChildren().remove(circle);
-            });
-
-            FadeTransition ft = new FadeTransition(new Duration(100), line);
-            ft.setFromValue(1);
-            ft.setToValue(0);
-            ft.setOnFinished(e -> {
-                overlay.getChildren().remove(line);
-            });
-
-            st.play();
-            ft.play();
+            overlay.getChildren().remove(circle);
         });
 
     }
--- Code/src/main/java/nl/utwente/ewi/caes/tactilefx/TouchDisplay.java
+++ Code/src/main/java/nl/utwente/ewi/caes/tactilefx/TouchDisplay.java
@@ -9,22 +9,25 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 class TouchDisplay extends Pane {
 
-    static Color COLOR_OPAQUE = new Color(1.0, 0, 0, 1);
-    static Color COLOR_SEMI_TRANSPARENT = new Color(1.0, 0, 0, 0.5);
-
+    final static Color COLOR_OPAQUE = new Color(1.0, 0, 0, 1);
+    final static Color COLOR_SEMI_TRANSPARENT = new Color(1.0, 0, 0, 0.5);
+    final static double touchCircleRadius = 50.0;
+    
     private final StringProperty labelText;
     private final Circle circle;
     private final Label label;
+    private final Line line;
     private final int touchId;
 
-    public TouchDisplay(double x, double y, double radius, int touchId) {
+    public TouchDisplay(double x, double y, Bounds bounds, int touchId) {
         this.touchId = touchId;
         labelText = new SimpleStringProperty(createLabelText(x, y, touchId));
 
-        circle = new Circle(radius, radius, radius);
+        circle = new Circle(touchCircleRadius, touchCircleRadius, touchCircleRadius);
         circle.setFill(Color.TRANSPARENT);
         circle.setStroke(COLOR_SEMI_TRANSPARENT);
         getChildren().add(circle);
@@ -32,16 +35,21 @@ class TouchDisplay extends Pane {
         label = new Label();
         label.textProperty().bindBidirectional(labelText);
         label.setTextFill(COLOR_OPAQUE);
-        label.relocate(0, radius * 2);
+        label.relocate(0, touchCircleRadius * 2);
         getChildren().add(label);
+        
+        line = new Line(x, y, bounds.getMinX(), bounds.getMinY());
+        getChildren().add(line);
 
         setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
     }
 
-    @Override
-    public void relocate(double x, double y) {
+    public void relocate(double x, double y, Bounds bounds) {
         labelText.set(createLabelText(x, y, touchId));
-
+        line.setStartX(x);
+        line.setStartY(y);
+        line.setEndX(bounds.getMinX());
+        line.setEndY(bounds.getMinY());
         Bounds cb = circle.getBoundsInParent();
         super.relocate(x - cb.getMinX() - cb.getWidth() / 2, y - cb.getMinY() - cb.getHeight() / 2);
     }
