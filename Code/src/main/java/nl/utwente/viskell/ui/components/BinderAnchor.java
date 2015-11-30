4f9f96d5b9ecae4c4522acfe1341240a44e0ff2d
Flumble <flumble@home.nl>
2015-11-30 20:56:46 +0100

    Fixed issues and resolved lamda's binderanchor

--- Code/src/main/java/nl/utwente/viskell/ui/components/BinderAnchor.java
+++ Code/src/main/java/nl/utwente/viskell/ui/components/BinderAnchor.java
@@ -3,40 +3,36 @@ package nl.utwente.viskell.ui.components;
 import nl.utwente.viskell.haskell.expr.Binder;
 import nl.utwente.viskell.haskell.expr.LetExpression;
 
-// TODO make this an independent class if other blocks (case?) need this too 
 /** An internal output anchor for an argument binder. */
 public class BinderAnchor extends OutputAnchor {
 
-    /**
-     * 
-     */
-    private final LambdaContainer lambdaContainer;
+    private final BlockContainer container;
 
-    // FIXME BinderAnchor should not have or use the DefinitionBlock parent
-    public BinderAnchor(LambdaContainer lambdaContainer, Block parent, Binder binder) {
+    // FIXME BinderAnchor should not have to use the Block parent
+    public BinderAnchor(BlockContainer container, Block parent, Binder binder) {
         super(parent, binder);
-        this.lambdaContainer = lambdaContainer;
+        this.container = container;
     }
 
     @Override
     protected void extendExprGraph(LetExpression exprGraph) {
-        return; // the scope of graph is limited its parent
+        return; // the scope of this graph is limited to its parent container
     }
     
     @Override
     public void initiateConnectionChanges() {
         // Starts a new (2 phase) change propagation process from this lambda.
-        this.lambdaContainer.handleConnectionChanges(false);
-        this.lambdaContainer.handleConnectionChanges(true);
+        container.handleConnectionChanges(false);
+        container.handleConnectionChanges(true);
     }
 
     @Override
     public void prepareConnectionChanges() {
-        this.lambdaContainer.refreshAnchorTypes();
+        this.container.refreshAnchorTypes();
     }
 
     @Override
     protected void handleConnectionChanges(boolean finalPhase) {
-        this.lambdaContainer.handleConnectionChanges(finalPhase);
+        this.container.handleConnectionChanges(finalPhase);
     }
 }
--- Code/src/main/java/nl/utwente/viskell/ui/components/ResultAnchor.java
+++ Code/src/main/java/nl/utwente/viskell/ui/components/ResultAnchor.java
@@ -7,27 +7,25 @@ import nl.utwente.viskell.haskell.expr.Expression;
 import nl.utwente.viskell.haskell.type.Type;
 import nl.utwente.viskell.haskell.type.TypeScope;
 
-// TODO make this an independent class if other blocks (case?) need this too
 /** An internal input anchor for a local result. */
 public class ResultAnchor extends InputAnchor {
-    /**
-     * 
-     */
-    private final LambdaContainer lambdaContainer;
+    
+    private final BlockContainer container;
+    
     /** The optional type of the result of the function (the last part of the signature). */
     private final Optional<Type> resType;
     
     // FIXME ResultAnchor should not have or use the DefinitionBlock parent
-    public ResultAnchor(LambdaContainer lambdaContainer, Block parent, Optional<Type> resType) {
+    public ResultAnchor(BlockContainer container, Block parent, Optional<Type> resType) {
         super(parent);
-        this.lambdaContainer = lambdaContainer;
+        this.container = container;
         this.resType = resType;
     }
     
     @Override
     public Expression getLocalExpr() {
-        if (this.resType.isPresent()) {
-            return new Annotated(super.getLocalExpr(), this.resType.get());
+        if (resType.isPresent()) {
+            return new Annotated(super.getLocalExpr(), resType.get());
         }
        
         return super.getLocalExpr();
@@ -35,15 +33,15 @@ public class ResultAnchor extends InputAnchor {
     
     /** Set fresh type for the next typechecking cycle.*/
     protected void refreshAnchorType(TypeScope scope) {
-        if (this.resType.isPresent()) {
-            this.setFreshRequiredType(this.resType.get(), scope);
+        if (resType.isPresent()) {
+            setFreshRequiredType(resType.get(), scope);
         } else {
-            this.setFreshRequiredType(TypeScope.unique("y"), scope);
+            setFreshRequiredType(TypeScope.unique("y"), scope);
         }
     }
 
     @Override
     protected void handleConnectionChanges(boolean finalPhase) {
-        this.lambdaContainer.handleConnectionChanges(finalPhase);
+        container.handleConnectionChanges(finalPhase);
     }
 }
