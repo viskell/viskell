221128603080fb70f87fdb84725cd181e4eaa5af
Wander Nauta <info@wandernauta.nl>
2015-12-04 14:05:06 +0100

    Clean up SimulateBlock a bit

--- Code/src/main/java/nl/utwente/viskell/ui/components/SimulateBlock.java
+++ Code/src/main/java/nl/utwente/viskell/ui/components/SimulateBlock.java
@@ -1,26 +1,36 @@
 package nl.utwente.viskell.ui.components;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import nl.utwente.viskell.ghcj.GhciSession;
 import nl.utwente.viskell.haskell.expr.Expression;
+import nl.utwente.viskell.haskell.type.Type;
 import nl.utwente.viskell.haskell.type.TypeScope;
 import nl.utwente.viskell.ui.ComponentLoader;
 import nl.utwente.viskell.ui.CustomUIPane;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
+/**
+ * A CLaSH-specific block that is a counterpart to the `simulate` function in Clash. Expects a function that turns a
+ * Signal of monotonically increasing numbers into a Signal of representable result values.
+ *
+ * While signals are infinite, SimulateBlock only shows the first N iterations/steps.
+ */
 public class SimulateBlock extends Block implements ComponentLoader {
     /** The Anchor that is used as input. */
     protected InputAnchor inputAnchor;
 
     /** The space containing the input anchor. */
-    @FXML
-    protected Pane inputSpace;
+    @FXML protected Pane inputSpace;
 
     /** The label on which to display type information. */
     @FXML protected Label inputType;
@@ -28,9 +38,9 @@ public class SimulateBlock extends Block implements ComponentLoader {
     /** The label on which to display the value of this block */
     @FXML protected Label value;
 
-    @FXML private SimpleIntegerProperty iteration;
+    @FXML protected SimpleIntegerProperty iteration;
 
-    @FXML protected Label iterationLabel;
+    @FXML protected Button iterationLabel;
 
     public SimulateBlock(CustomUIPane pane) {
         super(pane);
@@ -41,32 +51,35 @@ public class SimulateBlock extends Block implements ComponentLoader {
 
         iteration = new SimpleIntegerProperty(0);
         iteration.addListener(e -> this.invalidateVisualState());
-        iteration.addListener(e -> this.iterationLabel.setText(String.valueOf(iteration.get())));
+        iteration.addListener(e -> iterationLabel.setText(String.valueOf(iteration.get())));
     }
 
     @Override
     public final void invalidateVisualState() {
-        this.inputType.setText(this.inputAnchor.getStringType());
+        inputType.setText(inputAnchor.getStringType());
 
         if (inputAnchor.hasConnection()) {
-            try {
-                GhciSession ghciSession = getPane().getGhciSession();
-                String funName = "sim_fun_" + Integer.toHexString(this.hashCode());
-                ghciSession.push(funName, inputAnchor.getFullExpr());
-                String results = ghciSession.pullRaw("Data.List.take " + iteration.get() + " $ simulate " + funName + " [1..]").get();
-                value.setText(results);
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }
+            GhciSession ghciSession = getPane().getGhciSession();
+            String format = "Data.List.take %d $ simulate (%s) [1..]";
+            String expr = String.format(format, iteration.get(), inputAnchor.getFullExpr().toHaskell());
+            ListenableFuture<String> result = ghciSession.pullRaw(expr);
+
+            // See DisplayBlock.invalidateVisualState
+            Futures.addCallback(result, new FutureCallback<String>() {
+                public void onSuccess(String s)    { Platform.runLater(() -> value.setText(s)); }
+                public void onFailure(Throwable t) { Platform.runLater(() -> value.setText("?!?!?!")); }
+            });
         } else {
             value.setText("?");
         }
     }
 
+    /** Step to the next iteration. */
     @FXML private void step() {
         iteration.set(iteration.get() + 1);
     }
 
+    /** Reset to the first iteration. */
     @FXML private void reset() {
         iteration.set(0);
     }
@@ -88,6 +101,8 @@ public class SimulateBlock extends Block implements ComponentLoader {
 
     @Override
     public void refreshAnchorTypes() {
-        this.inputAnchor.setFreshRequiredType(getPane().getEnvInstance().buildType("Signal a -> Signal b"), new TypeScope());
+        String signature = "(Num a, Show b) => Signal a -> Signal b";
+        Type type = getPane().getEnvInstance().buildType(signature);
+        inputAnchor.setFreshRequiredType(type, new TypeScope());
     }
 }
