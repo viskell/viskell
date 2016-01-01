e7c032a06b256ac5ecce7a02b51616ece1ffa8ec
Arjan <w.a.boeijink@utwente.nl>
2016-01-01 20:59:00 +0100

    Add a few more functions from Data.Char/Ord/Function

--- Code/src/main/java/nl/utwente/viskell/ghcj/GhciEvaluator.java
+++ Code/src/main/java/nl/utwente/viskell/ghcj/GhciEvaluator.java
@@ -20,7 +20,7 @@ public class GhciEvaluator extends Evaluator {
 
     @Override
     protected List<String> getModules() {
-        return ImmutableList.of("Data.List", "Data.Maybe", "Data.Either", "Data.Monoid", "Control.Applicative", "Test.QuickCheck");
+        return ImmutableList.of("Data.List", "Data.Maybe", "Data.Either", "Data.Monoid", "Data.Char", "Data.Ord", "Data.Function", "Control.Applicative", "Test.QuickCheck");
     }
 
     @Override
