d6b3e8e035183934a1c80fca9d72aaa25e821f8d
Flumble <flumble@home.nl>
2016-01-07 22:08:30 +0100

    Add arguments to ChoiceBlock

--- Code/src/main/java/nl/utwente/viskell/ui/components/ChoiceBlock.java
+++ Code/src/main/java/nl/utwente/viskell/ui/components/ChoiceBlock.java
@@ -7,16 +7,17 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableList;
-
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import nl.utwente.viskell.haskell.expr.Binder;
 import nl.utwente.viskell.haskell.expr.Case;
 import nl.utwente.viskell.haskell.expr.Case.Alternative;
 import nl.utwente.viskell.haskell.expr.Expression;
 import nl.utwente.viskell.haskell.expr.LetExpression;
+import nl.utwente.viskell.haskell.expr.LocalVar;
 import nl.utwente.viskell.haskell.expr.Value;
 import nl.utwente.viskell.haskell.type.HaskellTypeError;
 import nl.utwente.viskell.haskell.type.Type;
@@ -25,6 +26,8 @@ import nl.utwente.viskell.haskell.type.TypeScope;
 import nl.utwente.viskell.haskell.type.TypeVar;
 import nl.utwente.viskell.ui.BlockContainer;
 import nl.utwente.viskell.ui.ToplevelPane;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * An evaluation block with multiple guarded alternatives.
@@ -46,6 +49,12 @@ public class ChoiceBlock extends Block {
     
     /** The container Node for the OutputAnchor */
     @FXML protected Pane resultSpace;
+    
+    /** The input space of for this choiceblock */
+    @FXML protected Pane inputSpace;
+    
+    /** The list of input anchors of this choiceblock */
+    protected List<ChoiceInputAnchor> inputAnchors;
 
     public ChoiceBlock(ToplevelPane pane) {
         super(pane);
@@ -53,8 +62,10 @@ public class ChoiceBlock extends Block {
         
         lanes = new ArrayList<>();
         output = new OutputAnchor(this, new Binder("choiceoutput"));
+        inputAnchors = new ArrayList<>();
         resultSpace.getChildren().add(output);
         dragContext.setGoToForegroundOnContact(false);
+        setMinHeight(USE_PREF_SIZE);
         
         addLane();
         addLane();
@@ -62,7 +73,7 @@ public class ChoiceBlock extends Block {
 
     @Override
     public List<InputAnchor> getAllInputs() {
-        return lanes.stream().map(Lane::getOutput).collect(Collectors.toList());
+        return inputAnchors.stream().map(i -> i.anchor).collect(Collectors.toList());
     }
 
     @Override
@@ -93,16 +104,30 @@ public class ChoiceBlock extends Block {
         // TODO make sure the last edited lane gets unified last to prevent
         // that large parts of a program become invalid in case of a type error,
         // but rather only the lane in which the edit took place.
-        TypeVar type = TypeScope.unique("lanetype");
+        List<TypeVar> typeList = new ArrayList<>();
+        TypeVar resultType = TypeScope.unique("choice_res");
+        for (int i = 0; i < inputAnchors.size(); ++i) {
+            typeList.add(TypeScope.unique("choice_arg"+i));
+        }
+        
         for (Lane lane : lanes) {
+            for (int i = 0; i < typeList.size(); ++i) {
+                try {
+                    TypeChecker.unify("choice block", typeList.get(i), lane.arguments.get(i).getType(Optional.empty()));
+                }
+                catch (HaskellTypeError e) {
+                }
+            }
             try {
-                TypeChecker.unify("choice block", type, lane.getOutput().getType());
+                TypeChecker.unify("choice block", resultType, lane.getOutput().getType());
             } catch (HaskellTypeError e) {
-                // TODO mark result anchors as invalid
             }
         }
 
-        output.setExactRequiredType(type);
+        for (int i = 0; i < typeList.size(); ++i) {
+            inputAnchors.get(i).anchor.setExactRequiredType(typeList.get(i));
+        }
+        output.setExactRequiredType(resultType);
     }
 
     public void handleConnectionChanges(boolean finalPhase) {
@@ -115,7 +140,16 @@ public class ChoiceBlock extends Block {
     @Override
     public Expression getLocalExpr(Set<OutputAnchor> outsideAnchors) {
         List<Alternative> bindings = lanes.stream().map(lane -> lane.getAlternative(outsideAnchors)).collect(Collectors.toList());
-        return new Case(new Value(Type.tupleOf(), "()"), bindings);
+        LetExpression let = new LetExpression(new Case(new Value(Type.tupleOf(), "()"), bindings), false);
+        lanes.forEach(lane -> {
+            for (int i = 0; i < getAllInputs().size(); ++i) {
+                int j = i;
+                getAllInputs().get(i).getOppositeAnchor().ifPresent(anchor -> let.addLetBinding(lane.arguments.get(j).binder, new LocalVar(anchor.binder)));
+            }
+        });
+        
+        getAllInputs().forEach(in -> in.getOppositeAnchor().ifPresent(out -> outsideAnchors.add(out)));
+        return let;
     }
 
     @Override
@@ -126,8 +160,9 @@ public class ChoiceBlock extends Block {
     @Override
     public void invalidateVisualState() {
         lanes.forEach(lane -> lane.invalidateVisualState());
+        inputAnchors.forEach(ChoiceInputAnchor::invalidateVisualState);
         signature.setText(output.getStringType());
-        this.output.invalidateVisualState();
+        output.invalidateVisualState();
     }
     
     @Override
@@ -139,8 +174,9 @@ public class ChoiceBlock extends Block {
     public void addLane() {
         Lane lane = new Lane(this);
         lanes.add(lane);
+        getAllInputs().forEach(anchor -> lane.addExtraInput());
         altSpace.getChildren().add(lane);
-        this.initiateConnectionChanges();
+        initiateConnectionChanges();
     }
     
     /** Removes the last alternative from this block */
@@ -156,6 +192,33 @@ public class ChoiceBlock extends Block {
     /** Returns the alternatives in this block */
     public List<Lane> getLanes() {
         return lanes;
+    }
+    
+    /** Adds extra input anchor to this block */
+    public void addExtraInput() {
+        ChoiceInputAnchor arg = new ChoiceInputAnchor();
+        inputAnchors.add(arg);
+        inputSpace.getChildren().add(arg);
+        lanes.forEach(Lane::addExtraInput);
+        
+        inputSpace.setMinHeight(35);
+        initiateConnectionChanges();
+    }
+
+    /** Removes the last input anchor of this block */
+    public void removeLastInput() {
+        if (!inputAnchors.isEmpty()) {
+            ChoiceInputAnchor arg = inputAnchors.remove(inputAnchors.size()-1);
+            arg.anchor.removeConnections();
+            inputSpace.getChildren().remove(arg);
+            lanes.forEach(Lane::removeLastInput);
+            
+            initiateConnectionChanges();
+        }
+        
+        if (inputAnchors.isEmpty()) {
+            inputSpace.setMinHeight(BASELINE_OFFSET_SAME_AS_HEIGHT);
+        }
     }
     
     @Override
@@ -189,4 +252,41 @@ public class ChoiceBlock extends Block {
         super.deleteAllLinks();
     }
 
+    /** Combined input anchor and type label. */
+    private class ChoiceInputAnchor extends VBox implements ConnectionAnchor.Target {
+
+        /** The connection anchor for this input argument. */
+        private final InputAnchor anchor;
+        
+        /** The input type label of this anchor. */
+        private final Label inputType;
+
+        private ChoiceInputAnchor() {
+            this.inputType = new Label(".....");
+            this.inputType.setMinWidth(USE_PREF_SIZE);
+            this.inputType.getStyleClass().add("inputType");
+            this.inputType.setAlignment(Pos.CENTER);
+            this.anchor = new InputAnchor(ChoiceBlock.this);
+            this.anchor.setAlignment(Pos.CENTER);
+            this.getChildren().addAll(this.anchor, this.inputType);
+            this.setTranslateY(-9);
+            this.setPickOnBounds(false);
+            this.setAlignment(Pos.CENTER);
+        }
+
+        @Override
+        public ConnectionAnchor getAssociatedAnchor() {
+            return this.anchor;
+        }
+
+        /** Refresh visual information such as types */
+        private void invalidateVisualState() {
+            this.anchor.invalidateVisualState();
+            boolean validConnection = this.anchor.hasValidConnection();
+            this.setTranslateY(validConnection ? 0 : -9);
+            this.inputType.setText(validConnection ? "zyxwv" : this.anchor.getStringType()); 
+            this.inputType.setVisible(!validConnection);
+        }
+    }
+    
 }
